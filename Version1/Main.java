7805777eaa524ccfabeb0ef862790c33a754243c
Eman Mahmoud <60985699+EmanMahm0ud@users.noreply.github.com>
2020-11-27 00:45:34 +0200

    Update Main.java

--- Version1/Main.java
+++ Version1/Main.java
@@ -1,10 +1,26 @@
-package main;
+import java.text.MessageFormat;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		English e = new English();
+		
+		e.addTemplete("forget password", "Dear {0}, your temporary password is {1}. Thanks for using our store.");
+		
+		e.addTemplete("order confirmation", "Dear {0}, your booking of the order {1} with order code {2} is confirmed. Thanks for using our store.");
+		
+		e.addTemplete("verification", "Dear {0}, your verification code is {1}. Thanks for using our store.");
+		
+		Channel channels = new SMS();
+		
+		User user = new Shopper();
+		user.setUserName("eman");
+		user.setChannels(channels);
+		user.setLanguageTemp(e);
+		
+		Notification n = new ForgetPassword();
+		n.sendNotification(user);
 	}
-
 }
