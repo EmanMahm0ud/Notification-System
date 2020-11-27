14270868d72ec29514934cfcfef98b2506fa4817
ReemEssam25 <55918672+ReemEssam25@users.noreply.github.com>
2020-11-27 13:42:46 +0200

    Add files via upload

--- Version1/SMS.java
+++ Version1/SMS.java
@@ -1,7 +1,7 @@
-
+package main;
 public class SMS implements Channel {
 	
 	public void sendNotification(String finalMessage) {
-		System.out.println(finalMessage);
+		System.out.println(finalMessage + "\t" + "*This is SMS channel*");
 	}
 }
