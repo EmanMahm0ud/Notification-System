2611ccb6f3857de602ffd97ee571dd1dbf9090c2
nada-raboh <63751876+nada-raboh@users.noreply.github.com>
2020-12-27 21:07:24 +0000

    Update Shopper.java

--- NotificationProject/src/main/java/model/NotificationProject/Shopper.java
+++ NotificationProject/src/main/java/model/NotificationProject/Shopper.java
@@ -6,6 +6,11 @@ public class Shopper extends User {
 	
 	private ArrayList<Order> orders;
 	
+	Shopper(){
+		orders = new ArrayList();
+		orders.add(new Order());
+	}
+	
 	public Order getLastOrder()
 	{
 		
