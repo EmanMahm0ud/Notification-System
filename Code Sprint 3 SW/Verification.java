0da6acc74cd2b22430a139f36dd7bcfeac7b7ac2
Samaa Muhammad <46351167+samaaMuhamad@users.noreply.github.com>
2021-01-08 15:35:16 +0200

    Other classes

---
+++ Code Sprint 3 SW/Verification.java
@@ -0,0 +1,23 @@
+package model.NotificationProject;
+
+import java.text.MessageFormat;
+import java.util.Random;
+
+public class Verification implements Notification {
+
+	@Override
+	public void sendNotification(User user, int id) {
+		// TODO Auto-generated method stub
+		
+		Random r = new Random();
+        Integer vCode = r.nextInt((100000 - 10000) + 1) + 10000;
+        
+        NotificationSystem notification = new NotificationSystem();
+		String message = notification.getNotification("verification", "English").getContent();
+		String finalTemplate = MessageFormat.format(message, user.getUserName(), vCode.toString());
+		
+		user.getChannels().addNotificationToQueue(finalTemplate);
+
+	}
+
+}
