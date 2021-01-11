cbae2bf791a4bb2c536ef8a0c7c6484a8dd6e811
ReemEssam25 <55918672+ReemEssam25@users.noreply.github.com>
2021-01-11 16:53:45 +0200

    Delete Channel.java

--- Code Sprint 2/Channel.java
+++
@@ -1,8 +0,0 @@
-package model.NotificationProject;
-
-public interface Channel {
-	public boolean isOnline();
-	public void setOnline(boolean online);
-	public void addNotificationToQueue(String finalMessage);
-	public void sendNotification();
-}
