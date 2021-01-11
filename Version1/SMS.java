0b27b59ea96d4afc6e0879e112d29180b17d1a84
ReemEssam25 <55918672+ReemEssam25@users.noreply.github.com>
2021-01-11 16:44:40 +0200

    Delete SMS.java

--- Version1/SMS.java
+++
@@ -1,7 +0,0 @@
-package main;
-public class SMS implements Channel {
-	
-	public void sendNotification(String finalMessage) {
-		System.out.println(finalMessage + "\t" + "*This is SMS channel*");
-	}
-}
